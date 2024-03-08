8746d19e6445a714bf467f52be5b70700b88e7e4
Karsten Jeppesen <kaje@ucn.dk>
2024-03-08 07:10:06 +0100

    SemaphoreDemo: Fixed - will run, but wrong
    Semaphores are missing

--- Session 04/SemaphoreDemo/src/ProCon.java
+++ Session 04/SemaphoreDemo/src/ProCon.java
@@ -9,80 +9,53 @@
 // release()
 //   will release an acquired semaphore
 
-
-import java.util.concurrent.*; 
+import java.util.concurrent.*;
 
 //A shared resource/class. 
-class Shared 
-{ 
-	static int count = 0; 
-} 
+class Shared {
+	static int count = 0;
+}
 
-class MyThread extends Thread 
-{
+class MyThread extends Thread {
 	// We need 2 semaphores.
 	// This one will prevent the Producer writing twice
 	Semaphore semPro;
 	// This one will prevent the consumer reading twice
 	Semaphore semCon;
 	String threadName;
-	
-	public MyThread(Semaphore mySemPro, Semaphore mySemCon, String threadName) 
-	{ 
+
+	public MyThread(Semaphore mySemPro, Semaphore mySemCon, String threadName) {
 		super(threadName);
 		// We set the semaphores from the main method
-		this.semPro = mySemPro; 
+		this.semPro = mySemPro;
 		this.semCon = mySemCon;
 		this.threadName = threadName;
 	}
-	
+
 	// Being lazy...
 	// Instead of coding Producer and Consumer separately
 	// They are here combined in one method
-	public void run()
-	{ 
-		
-		// run by thread "Producer" 
-		if(this.getName().equals("Producer")) 
-		{
-			System.out.println("Starting " + threadName);
-			for ( int i=0; i<5; i++)
-			{
-				try
-				{
-					Shared.count++; 
-					System.out.println(threadName + " Writes: " + Shared.count); 
-				}
-				catch (InterruptedException exc)
-				{
-					// Should anything fail - then  we end up here
-					System.out.println(exc); 
-				} 
-			}
-			// And we are done 
-			System.out.println("Ending " + threadName); 
-		} 
-		
-		// run by thread Consumer 
-		else
-		{ 
-			System.out.println("Starting " + threadName); 
-			for (int i=0; i< 5; i++)
-			{
-				try
-				{
-					System.out.println(threadName + " Reads: " + Shared.count);
-					// Signal the "Producer" that the value was read, so the buffer is now cleared
-				}
-				catch (InterruptedException exc)
-				{
-					// Should we fail - here we go
-					System.out.println(exc); 
-				} 
-			}
-			// And we are done 
-			System.out.println("Ending " + threadName);
-		} 
-	} 
-} 
+	public void run() {
 
+		// run by thread "Producer"
+		if (this.getName().equals("Producer")) {
+			System.out.println("Starting " + threadName);
+			for (int i = 0; i < 5; i++) {
+				Shared.count++;
+				System.out.println(threadName + " Writes: " + Shared.count);
+			}
+			// And we are done
+			System.out.println("Ending " + threadName);
+		}
+
+		// run by thread Consumer
+		else {
+			System.out.println("Starting " + threadName);
+			for (int i = 0; i < 5; i++) {
+				System.out.println(threadName + " Reads: " + Shared.count);
+			}
+			// And we are done
+			System.out.println("Ending " + threadName);
+		}
+	}
+}
